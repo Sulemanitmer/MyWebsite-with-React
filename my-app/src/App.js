f15cf31ec9e6d8e3326bdc4e65e12713cd50a0e4
Suleman Itmer <sulemanitmer@gmail.com>
2022-01-12 13:16:35 -0600

    Revert "fluid tester"
    This reverts commit 6abe1503004e0743cef740087ffa6c2e9c64cb67.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -21,7 +21,9 @@ function App() {
 
   return (
     <>
-     
+      <FluidAnimation
+        style={{ height: '100vh' }}
+      />
       <Navbar/>
       <div>
         {appNav === "home" && <Landing/>}
