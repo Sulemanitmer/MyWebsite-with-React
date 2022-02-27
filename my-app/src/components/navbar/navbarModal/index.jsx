9cc469c4d03ff37fe411d6258e56d248eb112f1e
Suleman Itmer <sulemanitmer@gmail.com>
2022-02-27 10:31:51 -0600

    testing new modal

--- my-app/src/components/navbar/navbarModal/index.jsx
+++ my-app/src/components/navbar/navbarModal/index.jsx
@@ -23,124 +23,151 @@ export default function NavBarModal() {
         dispatch(updateAppNav(navbarApp));
     }
 
+    const fadeType = navbarAppItem.navbarShowModal ? 'fadeIn' : 'fadeOut';
     return (
     <>
-        <Modal 
-            fullscreen={fullscreen}
-            show={navbarAppItem.navbarShowModal}
-            onHide={closeNavbarModal}
-            dialogClassName="custom-modal"
-            bsClass="my-modal"
-        >
+    //             <Spring
+    //                 delay={200}
+    //                 config={{ duration: 1000 }}
+    //                 from={{opacity:0}} 
+    //                 to={{opacity:0}} 
+    //             >
+    //                 {props => (
+    //                     <animated.div style={props} className="" >
+    //                     <Modal 
+    //         fullscreen={fullscreen}
+    //         show={navbarAppItem.navbarShowModal}
+    //     >
         
-        <Modal.Header  className='border-0 p-4'>
-          <div className='ms-auto' onClick={closeNavbarModal}>
-            <AiFillCloseCircle size={50} />
-          </div>
-            </Modal.Header>
-                <Modal.Body>
+    //     <Modal.Header  className='border-0 p-4'>
+    //       <div className='ms-auto' onClick={closeNavbarModal}>
+    //         <AiFillCloseCircle size={50} />
+    //       </div>
+    //         </Modal.Header>
+    //             <Modal.Body>
                 
-                <Spring
-                    from={{opacity:1, height:'0px'}} 
-                    to={{opacity:1, height: '100px'}} 
-                >
-                    {props => (
-                        <animated.div style={props} className="overflow-hidden position-fixed w-100">
-                        <Row style={{marginTop: "30px"}}>
-                            <Col>
-                                <a style={{fontSize: "54px"}}>AboutMe </a>
-                            </Col>
-                            <Col className='p-0 pt-2 mt-1'>
-                                <a style={{fontSize: "12.5px"}}>(Main)</a>
-                            </Col>
-                        </Row>
-                        </animated.div>
-                    )}
-                </Spring>
-                <Spring
-                    config={{ duration: 5000 }}
-                    from={{opacity:1, height:'100px'}} 
-                    to={{opacity:1, height: '200px'}} 
-                >
-                    {props => (
-                        <animated.div style={props} className="overflow-hidden position-fixed w-100">
-                            <Row style={{marginTop: "130px"}}>
-                                <Col>
-                                    <a style={{fontSize: "54px"}}>Portfolio </a>
-                                </Col>
-                                <Col className='p-0 pt-2 mt-1'>
-                                    <a style={{fontSize: "12.5px"}}>(Works</a>
-                                </Col>
-                            </Row>
-                        </animated.div>
-                    )}
-                </Spring>
-                <Spring
-                    config={{ duration: 5000 }}
-                    from={{opacity:1, height:'200px'}} 
-                    to={{opacity:1, height: '300px'}} 
-                >
-                    {props => (
-                        <animated.div style={props} className="overflow-hidden position-fixed w-100">
-                            <Row style={{marginTop: "230px"}}>
-                                <Col>
-                                    <a style={{fontSize: "54px"}}>Services </a>
-                                </Col>
-                                <Col className='p-0 pt-2 mt-1'>
-                                    <a style={{fontSize: "12.5px"}}>(Values)</a>
-                                </Col>
-                            </Row>
-                        </animated.div>
-                    )}
-                </Spring>
-                <Spring
-                    config={{ duration: 5000 }}
-                    from={{opacity:1, height:'300px'}} 
-                    to={{opacity:1, height: '400px'}} 
-                >
-                    {props => (
-                        <animated.div style={props} className="overflow-hidden position-fixed w-100">
-                            <Row style={{marginTop: "330px"}}>
-                                <Col>
-                                    <a style={{fontSize: "54px"}}>Contacts </a>
-                                </Col>
-                                <Col className='p-0 pt-2 mt-1'>
-                                    <a style={{fontSize: "12.5px"}}>(Contact Me)</a>
-                                </Col>
-                            </Row>
-                        </animated.div>
-                    )}
-                </Spring>
+    //             <Spring
+    //                 delay={200}
+    //                 config={{ duration: 1000 }}
+    //                 from={{opacity:1, height:'0px' , x : -20}} 
+    //                 to={{opacity:1, height: '100px' , x : 0}} 
+    //             >
+    //                 {props => (
+    //                     <animated.div style={props} className="overflow-hidden position-fixed w-100">
+    //                     <Row style={{marginTop: "30px"}}>
+    //                         <Col>
+    //                             <a style={{fontSize: "54px"}}>AboutMe </a>
+    //                         </Col>
+    //                         <Col className='p-0 pt-2 mt-1'>
+    //                             <a style={{fontSize: "12.5px"}}>(Main)</a>
+    //                         </Col>
+    //                     </Row>
+    //                     </animated.div>
+    //                 )}
+    //             </Spring>
+    //             <Spring
+    //                 delay={300}
+    //                 config={{ duration: 1000 }}
+    //                 from={{opacity:1, height:'100px' , x : -20}} 
+    //                 to={{opacity:1, height: '200px', x : 0}} 
+    //             >
+    //                 {props => (
+    //                     <animated.div style={props} className="overflow-hidden position-fixed w-100">
+    //                         <Row style={{marginTop: "130px"}}>
+    //                             <Col>
+    //                                 <a style={{fontSize: "54px"}}>Portfolio </a>
+    //                             </Col>
+    //                             <Col className='p-0 pt-2 mt-1'>
+    //                                 <a style={{fontSize: "12.5px"}}>(Works</a>
+    //                             </Col>
+    //                         </Row>
+    //                     </animated.div>
+    //                 )}
+    //             </Spring>
+    //             <Spring
+    //                 delay={400}
+    //                 config={{ duration: 1000 }}
+    //                 from={{opacity:1, height:'200px', x : -20}} 
+    //                 to={{opacity:1, height: '300px', x : 0}} 
+    //             >
+    //                 {props => (
+    //                     <animated.div style={props} className="overflow-hidden position-fixed w-100">
+    //                         <Row style={{marginTop: "230px"}}>
+    //                             <Col>
+    //                                 <a style={{fontSize: "54px"}}>Services </a>
+    //                             </Col>
+    //                             <Col className='p-0 pt-2 mt-1'>
+    //                                 <a style={{fontSize: "12.5px"}}>(Values)</a>
+    //                             </Col>
+    //                         </Row>
+    //                     </animated.div>
+    //                 )}
+    //             </Spring>
+    //             <Spring
+    //                 delay={500}
+    //                 config={{ duration: 1000 }}
+    //                 from={{opacity:1, height:'300px', x : -20}} 
+    //                 to={{opacity:1, height: '400px', x : 0}} 
+    //             >
+    //                 {props => (
+    //                     <animated.div style={props} className="overflow-hidden position-fixed w-100">
+    //                         <Row style={{marginTop: "330px"}}>
+    //                             <Col>
+    //                                 <a style={{fontSize: "54px"}}>Contacts </a>
+    //                             </Col>
+    //                             <Col className='p-0 pt-2 mt-1'>
+    //                                 <a style={{fontSize: "12.5px"}}>(Contact Me)</a>
+    //                             </Col>
+    //                         </Row>
+    //                     </animated.div>
+    //                 )}
+    //             </Spring>
                    
-            </Modal.Body>
-            <Modal.Footer className='d-inline justify-content-center pb-4 mb-4 border-0'>
-                <Row>
-                    <Col>
-                        <div>
-                            GitHub
-                        </div>
-                        <div>
-                            Linkedin
-                        </div>
-                        <div>
-                            214-680-7493
-                        </div>
-                    </Col>
-                    
-                    <Col>
-                        <div>
-                            Business inquiries
-                        </div>
-                        <div>
-                            sulemanitmer@gmail.com
-                        </div>
-                        <div>
-                            Work Smart not hard
-                        </div>
-                    </Col>
-                </Row>
-            </Modal.Footer>
-      </Modal>
+    //         </Modal.Body>
+    //         <Modal.Footer className='d-inline justify-content-center pb-4 mb-4 border-0'>
+    //         <Spring
+    //                 delay={1000}
+    //                 config={{ duration: 1500 }}
+    //                 from={{opacity:0}} 
+    //                 to={{opacity:1}} 
+    //             >
+    //                 {props => (
+    //                     <animated.div style={props}>
+    //                         <Row>
+    //                             <Col>
+    //                                 <div>
+    //                                     GitHub
+    //                                 </div>
+    //                                 <div>
+    //                                     Linkedin
+    //                                 </div>
+    //                                 <div>
+    //                                     214-680-7493
+    //                                 </div>
+    //                             </Col>
+                                
+    //                             <Col>
+    //                                 <div>
+    //                                     Business inquiries
+    //                                 </div>
+    //                                 <div>
+    //                                     sulemanitmer@gmail.com
+    //                                 </div>
+    //                                 <div>
+    //                                     Work Smart not hard
+    //                                 </div>
+    //                             </Col>
+    //                         </Row>
+    //                     </animated.div>
+    //                 )}
+    //             </Spring>
+                
+    //         </Modal.Footer>
+    //   </Modal>
+    //                     </animated.div>
+    //                 )}
+    //             </Spring>
+       
     </>
     )
 }
